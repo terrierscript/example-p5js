6f76d5d469dbbdda123282cfa5d4d278428d1d73
S. Suzuki <terrierscript@gmail.com>
2022-11-25 21:18:40 +0900

    update

--- src/Palette.tsx
+++ src/Palette.tsx
@@ -1,33 +1,36 @@
-import { useEffect, useLayoutEffect, useRef } from "react"
+import { FC, PropsWithChildren, useEffect, useLayoutEffect, useRef } from "react"
 import p5 from "p5"
 
-export const Palette = () => {
-  const ref = useRef<HTMLDivElement>(null)
-  const sketchRef = useRef<p5 | null>(null)
-  const sketch = (p: p5) => {
-    console.log(p)
-    p.setup = () => {
-      p.createCanvas(300, 300)
-      p.background(0)
-      p.fill(255)
-    }
-
-    p.draw = () => {
-      p.rect(100, 200, 50, 50)
-    }
+const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(300, 300)
+    p.background(0)
+    p.fill(255)
   }
+
+  p.draw = () => {
+  }
+  p.mouseMoved = () => {
+    p.rect(p.mouseX, p.mouseY, 50, 50)
+  }
+}
+
+export const Palette: FC<PropsWithChildren<{}>> = ({ children }) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const canvasRef = useRef<p5 | null>(null)
+
   useLayoutEffect(() => {
-    if (typeof window !== "object") {
-      return
-    }
     if (!ref.current) {
       return
     }
-    sketchRef.current = new p5(sketch, ref.current)
-    sketchRef.current.draw()
-    return () => {
-      sketchRef.current?.remove()
+    if (canvasRef.current) {
+      return
     }
-  }, [ref.current])
-  return <div ref={ref}></div>
+    canvasRef.current = new p5(sketch, ref.current)
+
+    return () => {
+      canvasRef.current?.remove()
+    }
+  }, [sketch])
+  return <div className="wrapper" ref={ref}>{children}</div>
 }
