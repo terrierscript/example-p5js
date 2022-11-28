3483db97694ef9ef4c733e290b01b7eab9fd8b52
S. Suzuki <terrierscript@gmail.com>
2022-11-29 00:00:03 +0900

    wip

--- src/Debug.tsx
+++ src/Debug.tsx
@@ -1,13 +1,14 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Grid } from "@chakra-ui/react"
 import { generateThree } from "./generateThree"
 
 export const Debug = () => {
   const colors = generateThree(1, 1)
   return <div>
     <div>debug</div>
-    {colors.map(c => {
-      return <Box bg={c.color} w={10} h={10}></Box>
-    })}
-    <Box></Box>
+    <Grid gridTemplateColumns={"repeat(100,1fr)"}>
+      {colors.map((c, i) => {
+        return <Box key={i} bg={c.color} w={10} h={10}></Box>
+      })}
+    </Grid>
   </div>
 }
--- src/generateThree.tsx
+++ src/generateThree.tsx
@@ -1,15 +1,22 @@
 export const generateThree = (cellX: number, cellY: number) => {
-  const arrA = Array.from({ length: 4 })
-  const arrB = Array.from({ length: 4 })
-  const arrC = Array.from({ length: 4 })
+  const arrA = Array.from({ length: 10 })
+  const arrB = Array.from({ length: 10 })
+  const arrC = Array.from({ length: 10 })
   return arrA.map((_, valA) => {
     return arrB.map((_, valB) => {
       return arrC.map((_, valC) => {
         const h = (valA / (arrA.length - 1) * 360).toFixed(0)
-        const l = (valB / (arrB.length - 1) * 100).toFixed(3)
-        const s = (valC / (arrC.length - 1) * 100).toFixed(3)
+        const l = (valA / (arrA.length - 1) * 360).toFixed(0)
+        const s = (valB / (arrB.length - 1) * 100).toFixed(3)
+        const hsl = `hsl(${h},${l}%,${s}%)`
+
+        const r = ((valA / (arrA.length - 1)) * 255).toFixed(0)
+        const g = ((valB / (arrB.length - 1)) * 255).toFixed(0)
+        const b = ((valC / (arrC.length - 1)) * 255).toFixed(0)
+        const rgb = `rgb(${r}, ${g}, ${b})`
+        console.log(rgb)
         return {
-          color: `hsl(${h},${l}%,${s}%)`,
+          color: rgb,
           x: (valA) * cellX,
           y: (valB + (valC * arrB.length)) * cellY,
           w: cellX,
